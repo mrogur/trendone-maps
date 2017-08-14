fde23d5ee46a27ae0a893635d5633da2e80a3434
pwlazlo <mrogur@gmail.com>
2017-08-14 13:26:47 +0200

    Working first version

--- trendone-maps.php
+++ trendone-maps.php
@@ -59,23 +59,8 @@ if ( ! class_exists( 'TrendOne_Maps' ) ) {
 				     apply_filters( 'widget_title', $this->title ) .
 				     $args['after_title'];
 			}
-			?>
+            include( plugin_dir_path( __FILE__ ) . '/trendone-widget.php');
 
-            <div class="row">
-                <div class="col-sm-5">
-                    aaa
-                </div>
-                <div class="col-sm-7">
-                    <iframe
-                            width="600"
-                            height="450"
-                            frameborder="0" style="border:0"
-                            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAYCNo-jLXCmXKeqJeJ1noJb2bxEOcXw6s&q=Bogucicka+2,+Katowice"
-                            allowfullscreen>
-                    </iframe>
-                </div>
-            </div>
-			<?php
 			echo $args['after_widget'];
 		}
 
@@ -102,159 +87,7 @@ if ( ! class_exists( 'TrendOne_Maps' ) ) {
 			if ( $instance ) {
 				$this->init_fields_from_instance( $instance );
 			}
-
-			?>
-            <!-- title -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'title' ) ?>">
-					<?php _e( "Title", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'title' ); ?>"
-                        type="text"
-                        value="<?php echo $this->title; ?>"
-                />
-            </p>
-            <!-- name -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'name' ) ?>">
-					<?php _e( "Name", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'name' ); ?>"
-                        type="text"
-                        value="<?php echo $this->trendoneName; ?>"
-                />
-            </p>
-            <!-- streetAddr -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'street_addr' ) ?>">
-					<?php _e( "Street Addr", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'street_addr' ); ?>"
-                        type="text"
-                        value="<?php echo $this->streetAddr; ?>"
-                />
-            </p>
-            <!-- postal -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'postal' ) ?>">
-					<?php _e( "Postal Code", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'postal' ); ?>"
-                        type="text"
-                        value="<?php echo $this->postalCode; ?>"
-                />
-            </p>
-
-            <!-- city -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'city' ) ?>">
-					<?php _e( "City", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'city' ); ?>"
-                        type="text"
-                        value="<?php echo $this->city; ?>"
-                />
-            </p>
-            <!-- directionTips -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'direction_tips' ) ?>">
-					<?php _e( "Direction Tips", 'trendone-maps' ) ?>
-                </label><br/>
-                <textarea
-                        rows="10"
-                        cols="5"
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'direction_tips' ); ?>"><?php echo $this->directionTips; ?></textarea>
-            </p>
-
-
-            <!-- emails -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'emails' ) ?>">
-					<?php _e( "Emails (comma sep.)", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'emails' ); ?>"
-                        type="text"
-                        value="<?php echo $this->emails; ?>"
-                />
-            </p>
-            <!-- phones -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'phones' ) ?>">
-					<?php _e( "Phones (comma sep.)", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'phones' ); ?>"
-                        type="text"
-                        value="<?php echo $this->phones; ?>"
-                />
-            </p>
-
-            <!-- apiKey -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'google_maps_api_key' ) ?>">
-					<?php _e( "Google Maps API Key", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'google_maps_api_key' ); ?>"
-                        type="text"
-                        value="<?php echo $this->apiKey; ?>"
-                />
-            </p>
-            <!-- mapWidth -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'map_width' ) ?>">
-					<?php _e( "Google Maps Map Width", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'map_width' ); ?>"
-                        type="text"
-                        value="<?php echo $this->mapWidth; ?>"
-                />
-            </p>
-            <!-- mapHeight -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'map_height' ) ?>">
-					<?php _e( "Google Maps Map Height", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'map_height' ); ?>"
-                        type="text"
-                        value="<?php echo $this->mapHeight; ?>"
-                />
-            </p>
-
-            <!-- queryString -->
-            <p class="trendone-form-element">
-                <label for="<?php echo $this->get_field_id( 'google_maps_query_string' ) ?>">
-					<?php _e( "Google Maps Query String", 'trendone-maps' ) ?>
-                </label><br/>
-                <input
-                        class="<?php echo $this->cssFormInputClasses; ?>"
-                        name="<?php echo $this->get_field_name( 'google_maps_query_string' ); ?>"
-                        type="text"
-                        value="<?php echo $this->queryString; ?>"
-                />
-            </p>
-
-			<?php
-
+            include( plugin_dir_path( __FILE__ ) . '/trendone-settings-form.php');
 		}
 
 		/**
@@ -274,6 +107,16 @@ if ( ! class_exists( 'TrendOne_Maps' ) ) {
 			$this->apiKey        = esc_attr( $instance['google_maps_api_key'] );
 			$this->queryString   = esc_attr( $instance['google_maps_query_string'] );
 		}
+
+		public function prepare_direction_tips() {
+			return nl2br($this->directionTips);
+		}
+
+		public function prepare_maps_url(  ) {
+			//https://www.google.com/maps/embed/v1/place?key=AIzaSyAYCNo-jLXCmXKeqJeJ1noJb2bxEOcXw6s&q=Bogucicka+2,+Katowice
+			return sprintf("https://www.google.com/maps/embed/v1/place?key=%s&q=%s", $this->apiKey, $this->queryString);
+
+		}
 	}
 
 
@@ -287,4 +130,7 @@ if ( ! class_exists( 'TrendOne_Maps' ) ) {
 	add_action( 'widgets_init', function () {
 		register_widget( "Trendone_Maps" );
 	} );
+
+	wp_register_style ( 'trendone-maps-style', plugins_url ( 'css/trendone-maps.css', __FILE__ ) );
+	wp_enqueue_style( 'trendone-maps-style' );
 }
--- trendone-settings-form.php
+++ trendone-settings-form.php
@@ -5,3 +5,152 @@
  * Date: 14.08.2017
  * Time: 12:07
  */
+
+?>
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'title' ) ?>">
+		<?php _e( "Title", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'title' ); ?>"
+            type="text"
+            value="<?php echo $this->title; ?>"
+    />
+</p>
+<!-- name -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'name' ) ?>">
+		<?php _e( "Name", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'name' ); ?>"
+            type="text"
+            value="<?php echo $this->trendoneName; ?>"
+    />
+</p>
+<!-- streetAddr -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'street_addr' ) ?>">
+		<?php _e( "Street Addr", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'street_addr' ); ?>"
+            type="text"
+            value="<?php echo $this->streetAddr; ?>"
+    />
+</p>
+<!-- postal -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'postal' ) ?>">
+		<?php _e( "Postal Code", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'postal' ); ?>"
+            type="text"
+            value="<?php echo $this->postalCode; ?>"
+    />
+</p>
+
+<!-- city -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'city' ) ?>">
+		<?php _e( "City", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'city' ); ?>"
+            type="text"
+            value="<?php echo $this->city; ?>"
+    />
+</p>
+<!-- directionTips -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'direction_tips' ) ?>">
+		<?php _e( "Direction Tips", 'trendone-maps' ) ?>
+    </label><br/>
+    <textarea
+            rows="10"
+            cols="5"
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'direction_tips' ); ?>"><?php echo $this->directionTips; ?></textarea>
+</p>
+
+
+<!-- emails -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'emails' ) ?>">
+		<?php _e( "Emails (comma sep.)", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'emails' ); ?>"
+            type="text"
+            value="<?php echo $this->emails; ?>"
+    />
+</p>
+<!-- phones -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'phones' ) ?>">
+		<?php _e( "Phones (comma sep.)", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'phones' ); ?>"
+            type="text"
+            value="<?php echo $this->phones; ?>"
+    />
+</p>
+
+<!-- apiKey -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'google_maps_api_key' ) ?>">
+		<?php _e( "Google Maps API Key", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'google_maps_api_key' ); ?>"
+            type="text"
+            value="<?php echo $this->apiKey; ?>"
+    />
+</p>
+<!-- mapWidth -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'map_width' ) ?>">
+		<?php _e( "Google Maps Map Width", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'map_width' ); ?>"
+            type="text"
+            value="<?php echo $this->mapWidth; ?>"
+    />
+</p>
+<!-- mapHeight -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'map_height' ) ?>">
+		<?php _e( "Google Maps Map Height", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'map_height' ); ?>"
+            type="text"
+            value="<?php echo $this->mapHeight; ?>"
+    />
+</p>
+
+<!-- queryString -->
+<p class="trendone-form-element">
+    <label for="<?php echo $this->get_field_id( 'google_maps_query_string' ) ?>">
+		<?php _e( "Google Maps Query String", 'trendone-maps' ) ?>
+    </label><br/>
+    <input
+            class="<?php echo $this->cssFormInputClasses; ?>"
+            name="<?php echo $this->get_field_name( 'google_maps_query_string' ); ?>"
+            type="text"
+            value="<?php echo $this->queryString; ?>"
+    />
+</p>
